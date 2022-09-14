8a44ee77c76ebc0258bba5da21e14da96d7169c1
yanagitani <takanori.yanagitani@gmail.com>
2022-09-15 08:01:44 +0900

    test: more coverage

---
+++ codec_test.go
@@ -0,0 +1,35 @@
+package sql2q
+
+import (
+	"context"
+	"testing"
+)
+
+func TestCodec(t *testing.T) {
+	t.Parallel()
+
+	t.Run("invalid", func(t *testing.T) {
+		t.Parallel()
+
+		t.Run("both", func(t *testing.T) {
+			t.Parallel()
+
+			_, e := CodecNew(nil, nil)
+			checkNg(t, e, func() string { return "Must fail" })
+		})
+
+		t.Run("pack", func(t *testing.T) {
+			t.Parallel()
+
+			_, e := CodecNew(nil, func(_c context.Context, _m Msg) ([]Msg, error) { return nil, nil })
+			checkNg(t, e, func() string { return "Must fail" })
+		})
+
+		t.Run("unpack", func(t *testing.T) {
+			t.Parallel()
+
+			_, e := CodecNew(func(_c context.Context, _m []Msg) (Msg, error) { return MsgNew(-1, nil), nil }, nil)
+			checkNg(t, e, func() string { return "Must fail" })
+		})
+	})
+}
